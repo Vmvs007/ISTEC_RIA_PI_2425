2f68282c52233b22576aff93455967a3626f1c0e
Vitor Santos <vmvs007@gmail.com>
2025-03-17 09:52:03 +0000

    Dicionários

---
+++ FichasPraticasISTEC/FichaPratica07/Ex_01.py
@@ -0,0 +1,13 @@
+nomeUtilizador = input("Insira o seu nome: ")
+idadeUtilizador = int(input("Insira a sua idade: "))
+cidadeUtilizador = input("Insira a sua cidade: ")
+
+utilizador = {
+    "nome": nomeUtilizador,
+    "idade": idadeUtilizador,
+    "cidade": cidadeUtilizador
+}
+
+print(utilizador)
+# print("Cidade:",utilizador["cidade"])
+
